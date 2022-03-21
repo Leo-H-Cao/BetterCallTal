cb83adc084e8f37ef291084adf36e5c41066bd71
Vincent Chen <vjc9@duke.edu>
2022-03-21 19:23:29 -0400

    added Board, Piece, Tile APIs

--- doc/plan/api/Piece.java
+++ doc/plan/api/Piece.java
@@ -2,4 +2,10 @@ package oogasalad;
 
 public interface Piece {
 
+  /***
+   * Moves piece to square
+   * @param tile to move to
+   * @return map of updated squares
+   */
+  Map<Tile, Piece> move(Tile tile);
 }
---
+++ doc/plan/api/Tile.java
@@ -0,0 +1,10 @@
+package oogasalad;
+
+public interface Tile {
+
+  /***
+   * Updates piece on tile
+   * @param newPiece to place on tile
+   */
+  void update(Piece newPiece);
+}
