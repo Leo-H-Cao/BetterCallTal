47e8603c99853ebe5f9453ad57ce73652531c8be
Vincent Chen <vjc9@duke.edu>
2022-03-25 17:54:30 -0400

    fixed random character that caused a compiler error #1

--- src/main/java/oogasalad/GamePlayer/Board/ChessBoard.java
+++ src/main/java/oogasalad/GamePlayer/Board/ChessBoard.java
@@ -1,13 +1,21 @@
 package oogasalad.GamePlayer.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import oogasalad.GamePlayer.GamePiece.Piece;
 
 public class ChessBoard {
 
+  private ChessTile[][] board;
+  private TurnCriteria turnCriteria;
+  private int numTeams;
+  private List<EndCondition> endConditions;
   public ChessBoard(int length, int height, TurnCriteria turnCriteria, int numTeams, List<EndCondition> endConditions) {
-
+    board = new ChessTile[length][height];
+    this.turnCriteria = turnCriteria;
+    this.numTeams = numTeams;
+    this.endConditions = endConditions;
   }
   /***
    * Moves the piece on fromSquare to toSquare
