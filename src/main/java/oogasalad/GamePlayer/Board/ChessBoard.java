8e982ad4b3793febe8bbecd6b9d6788d6f03abf7
Vincent Chen <vjc9@duke.edu>
2022-03-25 17:41:09 -0400

    moved JSON config files to /data

--- src/main/java/oogasalad/GamePlayer/Board/ChessBoard.java
+++ src/main/java/oogasalad/GamePlayer/Board/ChessBoard.java
@@ -1,6 +1,20 @@
 package oogasalad.GamePlayer.Board;
 
+import java.util.Set;
+import oogasalad.GamePlayer.GamePiece.Piece;
+
 public class ChessBoard {
+
+  /***
+   * Moves the piece on fromSquare to toSquare
+   *
+   * @param piece to move
+   * @param toSquare end square
+   * @return set of updated tiles
+   */
+  public Set<ChessTile> move(Piece piece, ChessTile toSquare) {
+    return null;
+  }
 
 
 }
