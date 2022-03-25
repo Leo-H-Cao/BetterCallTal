30a46c4325188a1d872e95541c8252cb919d2bd4
Vincent Chen <vjc9@duke.edu>
2022-03-25 18:20:34 -0400

    changed movement class to also take board as a parameter when getting moves/moving

--- src/main/java/oogasalad/GamePlayer/Movement/Movement.java
+++ src/main/java/oogasalad/GamePlayer/Movement/Movement.java
@@ -2,6 +2,7 @@ package oogasalad.GamePlayer.Movement;
 
 import java.util.List;
 import java.util.Set;
+import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.ChessTile;
 import oogasalad.GamePlayer.GamePiece.Piece;
 
@@ -21,9 +22,10 @@ public class Movement {
    *
    * @param piece to move
    * @param finalSquare end square
+   * @param board to move on
    * @return set of updated tiles
    */
-  public Set<ChessTile> movePiece(Piece piece, ChessTile finalSquare) {
+  public Set<ChessTile> movePiece(Piece piece, ChessTile finalSquare, ChessBoard board) {
     return null;
   }
 
@@ -31,9 +33,10 @@ public class Movement {
    * Returns all possible moves a piece can make
    *
    * @param piece to get moves from
+   * @param board to move on
    * @return set of tiles the piece can move to
    */
-  public Set<ChessTile> getMoves(Piece piece) {
+  public Set<ChessTile> getMoves(Piece piece, ChessBoard board) {
     return null;
   }
 }
