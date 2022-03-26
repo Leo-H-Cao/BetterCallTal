07b0745b3a5f4fd61f6fb3cbbd990303e5964321
Ritvik Janamsetty <janamsetty@gmail.com>
2022-03-25 23:51:13 -0400

    Created all files

--- doc/plan/api/server/History.java
+++ doc/plan/api/server/History.java
@@ -10,7 +10,7 @@ public interface History {
    * Adds a new state to the history.
    * @param newState The new state to add.
    */
-  public void addToHistory(BoardState newState);
+  public void addToHistory(State newState);
 
   /**
    * Returns the size of the history.
@@ -23,48 +23,44 @@ public interface History {
    * @param index The index of the state to return.
    * @return the state at a given index.
    */
-  public BoardState getState(int index);
+  public State getState(int index);
 
   /**
    * Returns the most recent state.
    * @return the most recent state.
    */
-  public BoardState getCurrentState();
+  public State getCurrentState();
 
   /**
    * Returns the index of the most recent state.
    * @return the index of the most recent state.
    */
-  public BoardState getCurrentIndex();
+  public State getCurrentIndex();
 
   /**
    * Rewinds the history by amount of states.
    * @param amount The number of states to rewind.
    * @return the state that was rewound to.
    */
-  public BoardState rewindBackStates(int amount);
+  public State rewindBackStates(int amount);
 
   /**
    * Advances the history by amount of states.
    * @param amount The number of states to advance.
    * @return the state that was advanced to.
    */
-  public BoardState advanceForwardStates(int amount);
+  public State advanceForwardStates(int amount);
 
   /**
    * Rewinds the history to a certain index.
    * @param index The index to rewind to.
    * @return the state that was rewound to.
    */
-  public BoardState goToState(int index);
+  public State goToState(int index);
 
   /**
    * Clears the history.
    */
   public void clearHistory();
-
-
-
-
 
 }
