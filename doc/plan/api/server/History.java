dd0fa65a0d72ab8a8b8a317d1c692188be9e97fb
Ritvik Janamsetty <janamsetty@gmail.com>
2022-03-26 00:05:43 -0400

    Refactored to make State and Rules JSON objects

--- doc/plan/api/server/History.java
+++ doc/plan/api/server/History.java
@@ -1,5 +1,7 @@
 package oogasalad.server;
 
+import org.json.simple.JSONObject;
+
 /**
  * This class is used to store prior states of the game, allowing for checking complex game
  * interactions and rollbacking to previous states.
@@ -8,55 +10,63 @@ public interface History {
 
   /**
    * Adds a new state to the history.
+   *
    * @param newState The new state to add.
    */
   public void addToHistory(State newState);
 
   /**
    * Returns the size of the history.
+   *
    * @return the size of the history.
    */
   public int getHistorySize();
 
   /**
    * Returns the state at a given index.
+   *
    * @param index The index of the state to return.
    * @return the state at a given index.
    */
-  public State getState(int index);
+  public JSONObject getState(int index);
 
   /**
    * Returns the most recent state.
+   *
    * @return the most recent state.
    */
-  public State getCurrentState();
+  public JSONObject getCurrentState();
 
   /**
    * Returns the index of the most recent state.
+   *
    * @return the index of the most recent state.
    */
-  public State getCurrentIndex();
+  public int getCurrentIndex();
 
   /**
    * Rewinds the history by amount of states.
+   *
    * @param amount The number of states to rewind.
    * @return the state that was rewound to.
    */
-  public State rewindBackStates(int amount);
+  public JSONObject rewindBackStates(int amount);
 
   /**
    * Advances the history by amount of states.
+   *
    * @param amount The number of states to advance.
    * @return the state that was advanced to.
    */
-  public State advanceForwardStates(int amount);
+  public JSONObject advanceForwardStates(int amount);
 
   /**
    * Rewinds the history to a certain index.
+   *
    * @param index The index to rewind to.
    * @return the state that was rewound to.
    */
-  public State goToState(int index);
+  public JSONObject goToState(int index);
 
   /**
    * Clears the history.
