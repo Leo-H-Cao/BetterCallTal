79da95045d9e42508c7543fc103db15dc1a8ccc8
Jed Yang <zy111@duke.edu>
2022-03-27 12:20:47 -0400

    created an empty rows x columns size ChessBoard from JSON file board.json

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ open module oogasalad_app {
     requires javafx.base;
     requires javafx.media;
     requires javafx.web;
+    requires org.json;
 
     // allow other classes to access listed packages in your project
     exports oogasalad;
--- src/main/java/oogasalad/GamePlayer/BoardSetup.java
+++ src/main/java/oogasalad/GamePlayer/BoardSetup.java
@@ -1,6 +1,32 @@
 package oogasalad.GamePlayer;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import oogasalad.GamePlayer.Board.ChessBoard;
+import org.json.JSONObject;
+
 public class BoardSetup {
+  private ChessBoard myBoard;
+  private JSONObject myJSONObject;
+
+  //TODO: create custom exception for handling IOException
+  public BoardSetup(String JSONFileName) throws IOException {
+    String content = new String(Files.readAllBytes(Path.of(JSONFileName)));
+    myJSONObject = new JSONObject(content);
+  }
+
+  public ChessBoard createBoard(){
+    int rows = Integer.parseInt(myJSONObject.getJSONArray("general").getJSONObject(0).get("rows").toString());
+    int columns = Integer.parseInt(myJSONObject.getJSONArray("general").getJSONObject(0).get("columns").toString());
+    myBoard = new ChessBoard(rows, columns, null ,2, null);
+    return myBoard;
+  }
 
 
+  public static void main(String[] args) throws IOException {
+    BoardSetup a = new BoardSetup("data/GameEngineResources/board.json");
+    System.out.println(a.myJSONObject.getJSONArray("general").getJSONObject(0).get("rows"));
+  }
 }
