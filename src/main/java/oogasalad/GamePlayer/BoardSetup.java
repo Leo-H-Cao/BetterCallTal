9e42292e4d11024df82cc7aa9e91da90c2160fc1
Jed Yang <zy111@duke.edu>
2022-03-28 18:21:54 -0400

    generalized move and capture format in board.json

--- src/main/java/oogasalad/GamePlayer/BoardSetup.java
+++ src/main/java/oogasalad/GamePlayer/BoardSetup.java
@@ -4,8 +4,10 @@ package oogasalad.GamePlayer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.Player;
+import oogasalad.GamePlayer.Movement.Movement;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -24,6 +26,7 @@ public class BoardSetup {
     int columns = Integer.parseInt(myJSONObject.getJSONArray("general").getJSONObject(0).get("columns").toString());
 
     myBoard = new ChessBoard(rows, columns, null ,getPlayers(), null);
+    getMovements();
     return myBoard;
   }
 
@@ -32,13 +35,22 @@ public class BoardSetup {
     Player[] players = new Player[rawData.length()];
     for(int i=0; i<players.length; i++) {
 
-      players[i] = new Player((Integer) rawData.getJSONObject(i).get("team"), (int[]) rawData.getJSONObject(i).get("opponents"));
+      //TODO: Fix this line (cannot cast to int[])
+      //players[i] = new Player((Integer) rawData.getJSONObject(i).get("team"), (int[]) rawData.getJSONObject(i).get("opponents"));
     }
     return players;
   }
 
+  private List<Movement> getMovements(){
+    JSONArray a = myJSONObject.getJSONArray("pieces").getJSONObject(0).getJSONArray("movements").getJSONArray(0);
+    System.out.println(a);
+    System.out.println("HI");
+    return null;
+  }
+
   public static void main(String[] args) throws IOException {
     BoardSetup a = new BoardSetup("data/GameEngineResources/board.json");
+    a.createBoard();
     System.out.println(a.myJSONObject.getJSONArray("general").getJSONObject(0).get("rows"));
   }
 }
