b9fdce4d698478441a11dbe55b779475f96b051a
Jose Santillan <mezqi14@icloud.com>
2022-03-28 19:48:13 -0400

    added some javadoc and comments

---
+++ src/test/java/oogasalad/backend/TileTests.java
@@ -0,0 +1,24 @@
+package oogasalad.backend;
+
+import oogasalad.Main;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TileTests {
+
+
+  @BeforeEach
+  void setUp() {
+    Main m = new Main();
+  }
+  /**
+   * This test checks to see if moving a horse from its original position
+   * (in original chess) moves correctly and the respective tiles are updated
+   */
+  @Test
+  void containsHorse() {
+
+  }
+
+
+}
