6cfb71f59bc82ee34dd972c941b8411fcbabca2d
Vincent Chen <vjc9@duke.edu>
2022-03-28 22:18:20 -0400

    added two examples for turncriteria: linear and constantly increasing #021

--- src/main/java/oogasalad/GamePlayer/Board/ChessBoard.java
+++ src/main/java/oogasalad/GamePlayer/Board/ChessBoard.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import oogasalad.GamePlayer.Board.TurnCriteria.TurnCriteria;
 import oogasalad.GamePlayer.EngineExceptions.InvalidMoveException;
 import oogasalad.GamePlayer.EngineExceptions.OutsideOfBoardException;
 import oogasalad.GamePlayer.EngineExceptions.WrongPlayerException;
