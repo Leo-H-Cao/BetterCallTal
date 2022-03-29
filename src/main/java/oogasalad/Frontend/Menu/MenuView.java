6a99ac09646982caf2c0b8e0c9a6e45190994315
Luke McSween <luke.mcsween@duke.edu>
2022-03-29 15:36:48 -0400

    Created MenuView class

---
+++ src/main/java/oogasalad/Frontend/Menu/MenuView.java
@@ -0,0 +1,14 @@
+package oogasalad.Frontend.Menu;
+
+/**
+ * This class will handle all Windows that are not GameEditor or GameView (basically all set up pop-ups).
+ */
+
+import javafx.stage.Stage;
+
+public class MenuView {
+
+    public MenuView(Stage stage) {
+
+    }
+}
