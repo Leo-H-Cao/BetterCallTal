5e9db54b4c87ce5ede6da90d4acabc9f2cf9aaf7
Luke McSween <luke.mcsween@duke.edu>
2022-03-29 15:28:59 -0400

    Created GameView class

---
+++ src/main/java/oogasalad/Frontend/Game/GameView.java
@@ -0,0 +1,15 @@
+package oogasalad.Frontend.Game;
+
+import javafx.stage.Stage;
+
+/**
+ * This class will handle the View for the game being played.
+ */
+
+
+public class GameView {
+
+    public GameView(Stage stage) {
+
+    }
+}
