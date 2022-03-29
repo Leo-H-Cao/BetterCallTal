ea435d4889c5285a1db759cb0789d7ed12539292
Luke McSween <luke.mcsween@duke.edu>
2022-03-29 15:14:38 -0400

    Main creates controller

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -4,20 +4,37 @@ package oogasalad;
 import javafx.stage.Stage;
 import oogasalad.Frontend.MainView;
 import javafx.application.Application;
+import oogasalad.controller.Controller;
+import java.util.List;
 
+import java.util.ArrayList;
 /**
- * Feel free to completely change this code or delete it entirely. 
+ * Main class to be run by the user, will launch the program.
  */
+
 public class Main extends Application {
+
+    private static List<Controller> myControllers;
+    public static final String TITLE = "Better Call Tal";
+
     /**
-     * A method to test (and a joke :).
+     * Start of the program.
      */
-    public double getVersion () {
-        return 0.001;
+    @Override
+    public void start (Stage stage) {
+        myControllers = new ArrayList<>();
+        myControllers.add(new Controller(stage, ()->addController()));
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        new MainView(primaryStage);
+    /**
+     * Add a controller when the user clicks new window. Creates a new stage and passes it to new controller.
+     */
+    private void addController() {
+        Stage newStage = new Stage();
+        String numWindow = "" + myControllers.size() + 1;
+        newStage.setTitle(TITLE + numWindow);
+        newStage.show();
+        Runnable runHandler = ()-> addController();
+        myControllers.add(new Controller(newStage, runHandler));
     }
 }
--- src/main/java/oogasalad/controller/Controller.java
+++ src/main/java/oogasalad/controller/Controller.java
@@ -1,4 +1,10 @@
 package oogasalad.controller;
 
+import javafx.stage.Stage;
+
 public class Controller {
+
+    public Controller(Stage stage, Runnable newControllerHandler) {
+
+    }
 }
