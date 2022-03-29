47ce8761840a68079cf88010fccdc05c2e20865d
Luke McSween <luke.mcsween@duke.edu>
2022-03-29 15:34:25 -0400

    Created the GameEditorView class

---
+++ src/main/java/oogasalad/Frontend/Editor/GameEditorView.java
@@ -0,0 +1,13 @@
+package oogasalad.Frontend.Editor;
+/**
+ * This class will handle the view for the Game Editor.
+ */
+
+import javafx.stage.Stage;
+
+public class GameEditorView {
+
+    public GameEditorView(Stage stage) {
+
+    }
+}
