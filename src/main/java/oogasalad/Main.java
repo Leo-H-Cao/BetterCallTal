ccfb512cf88b014a14c7bd66a5ca5b2657da8f98
Luke McSween <luke.mcsween@duke.edu>
2022-03-30 23:26:43 -0400

    fixed main to avoid merge issues

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -3,37 +3,20 @@ package oogasalad;
 
 import javafx.stage.Stage;
 import javafx.application.Application;
-import oogasalad.controller.Controller;
-import java.util.List;
+import oogasalad.Frontend.Menu.LanguageModal;
 
-import java.util.ArrayList;
 /**
- * Main class to be run by the user, will launch the program.
+ * Main class to be run by the user, will launch a language modal splash screen which will then launch the modal program.
  */
 
 public class Main extends Application {
-
-    private static List<Controller> myControllers;
-    public static final String TITLE = "Better Call Tal";
 
     /**
      * Start of the program.
      */
     @Override
     public void start (Stage stage) {
-        myControllers = new ArrayList<>();
-        myControllers.add(new Controller(stage, ()->addController()));
+        LanguageModal langmodal = new LanguageModal(stage);
     }
 
-    /**
-     * Add a controller when the user clicks new window. Creates a new stage and passes it to new controller.
-     */
-    private void addController() {
-        Stage newStage = new Stage();
-        String numWindow = "" + myControllers.size() + 1;
-        newStage.setTitle(TITLE + numWindow);
-        newStage.show();
-        Runnable runHandler = () -> addController();
-        myControllers.add(new Controller(newStage, runHandler));
-    }
 }
