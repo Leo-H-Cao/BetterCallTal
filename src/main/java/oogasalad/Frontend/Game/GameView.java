3d5279855acf34aa7b0d20d3de22c26de7a4d6b5
Luke McSween <luke.mcsween@duke.edu>
2022-03-31 14:18:22 -0400

    SetUpBoard API skeleton in place

--- src/main/java/oogasalad/Frontend/Game/GameView.java
+++ src/main/java/oogasalad/Frontend/Game/GameView.java
@@ -1,6 +1,10 @@
 package oogasalad.Frontend.Game;
 
 import javafx.stage.Stage;
+import oogasalad.GamePlayer.Board.ChessTile;
+import oogasalad.GamePlayer.GamePiece.Piece;
+
+import java.util.Collection;
 
 /**
  * This class will handle the View for the game being played.
@@ -9,9 +13,25 @@ import javafx.stage.Stage;
 
 public class GameView {
 
+    private Collection<ChessTile> myBoard;
+    private Collection<Piece> myPieces;
+
     private Stage myStage;
 
     public GameView(Stage stage) {
         myStage = stage;
     }
+
+    /**
+     * setUpBoard method will be called when the user hosts a game and uploads a JSon game file.
+     * The back-end will parse the information and call this method with objects that contain
+     * where the starting pieces, power ups, etc. go, and any other information relavant to
+     * setting up the board.
+     */
+
+    public void SetUpBoard(Collection<ChessTile> board, Collection<Piece> pieces) {
+        myBoard = board;
+        myPieces = pieces;
+    }
+
 }
