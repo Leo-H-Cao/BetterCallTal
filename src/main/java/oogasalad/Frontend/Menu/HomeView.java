4a3bd5d6b043df9a0e6afbd05224c746a7a4cc1f
Luke McSween <luke.mcsween@duke.edu>
2022-03-31 19:48:48 -0400

    added buttons to HomeView

--- src/main/java/oogasalad/Frontend/Menu/HomeView.java
+++ src/main/java/oogasalad/Frontend/Menu/HomeView.java
@@ -3,6 +3,8 @@ package oogasalad.Frontend.Menu;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -29,6 +31,23 @@ public class HomeView extends SceneView {
         VBox vBox = new VBox();
         vBox.getChildren().add(t);
         myRoot.getChildren().add(vBox);
+
+        Button[] buttons = makeButtons();
+        VBox.setVgrow(buttons[0], Priority.ALWAYS);
+        VBox.setVgrow(buttons[1], Priority.ALWAYS);
+        VBox.setVgrow(buttons[2], Priority.ALWAYS);
+
+        vBox.getChildren().addAll(buttons);
+
         return scene;
+    }
+
+    private Button[] makeButtons() {
+        Button[] ret = new Button[3];
+        for (int i=0; i <3; i++) {
+            ret[i] = new Button(String.valueOf(i));
+            ret[i].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        }
+        return ret;
     }
 }
