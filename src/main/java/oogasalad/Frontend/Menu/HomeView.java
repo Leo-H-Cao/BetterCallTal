54f9e867edd2cbbc095197359f0a7b4533bb3299
Luke McSween <luke.mcsween@duke.edu>
2022-03-31 22:50:22 -0400

    Tweaked HomeView

--- src/main/java/oogasalad/Frontend/Menu/HomeView.java
+++ src/main/java/oogasalad/Frontend/Menu/HomeView.java
@@ -1,14 +1,14 @@
 package oogasalad.Frontend.Menu;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import oogasalad.Frontend.MainView;
 import oogasalad.Frontend.SceneView;
-
-import java.util.Arrays;
 
 /**
  * HomeView class will handle the navigation of the User from the home screen to the next page they seek.
@@ -18,8 +18,11 @@ import java.util.Arrays;
 public class HomeView extends SceneView {
 
     private VBox myVbox;
-    private static final String[] ButtonOptions = {"HomeViewCreate", "HomeViewJoin", "HomeViewHost"};
-    private static final String TITLE = "HomeViewTitle";
+    private static final String RESOURCE_PATH = "HomeView";
+    private static final String[] ButtonOptions = {"Create", "Join", "Host"};
+    private static final String TITLE = "Title";
+    private static final int VBOX_SPACING = 5;
+    private static final int SCREEN_SIZE = 500;
 
     public HomeView() {
     }
@@ -27,22 +30,22 @@ public class HomeView extends SceneView {
     @Override
     protected Scene makeScene() {
 
-        Scene scene = new Scene(myRoot);
-        myVbox = new VBox();
+        Scene scene = new Scene(myRoot, 500, 500);
+        myVbox = new VBox(VBOX_SPACING);
+        myVbox.setPrefSize(SCREEN_SIZE, SCREEN_SIZE);
 
-        myRoot.prefWidth(500);
-        myRoot.prefHeight(700);
 
-        Text t = new Text(MainView.getLanguage().getString(TITLE));
+        Text t = new Text(MainView.getLanguage().getString(RESOURCE_PATH + TITLE));
         t.setFont(new Font(64));
+        t.setWrappingWidth(SCREEN_SIZE);
         myVbox.getChildren().add(t);
 
         Button[] buttons = makeButtons();
 
-        // TODO: Figure out setVgrow
-        // VBox.setVgrow(buttons[0], Priority.ALWAYS);
-        // VBox.setVgrow(buttons[1], Priority.ALWAYS);
-        // VBox.setVgrow(buttons[2], Priority.ALWAYS);
+        //TODO: Figure out setVgrow
+        //VBox.setVgrow(buttons[0], Priority.ALWAYS);
+        //VBox.setVgrow(buttons[1], Priority.ALWAYS);
+        //VBox.setVgrow(buttons[2], Priority.ALWAYS);
 
         myVbox.getChildren().addAll(buttons);
         myRoot.getChildren().add(myVbox);
@@ -53,8 +56,9 @@ public class HomeView extends SceneView {
     private Button[] makeButtons() {
         Button[] ret = new Button[ButtonOptions.length];
         for (int i=0; i < ButtonOptions.length; i++) {
-            ret[i] = new Button(MainView.getLanguage().getString(ButtonOptions[i]));
-            ret[i].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            ret[i] = new Button(MainView.getLanguage().getString(RESOURCE_PATH + ButtonOptions[i]));
+            ret[i].setPrefSize(SCREEN_SIZE, SCREEN_SIZE);
+            ret[i].setAlignment(Pos.CENTER);
         }
         return ret;
     }
