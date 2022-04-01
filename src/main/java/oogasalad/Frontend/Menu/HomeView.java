e6380b0e42008c503f9f0b7b88d942d46d2b6c35
Luke McSween <luke.mcsween@duke.edu>
2022-03-31 20:53:47 -0400

    Fixed up HomeView, implemented resource bundles

--- src/main/java/oogasalad/Frontend/Menu/HomeView.java
+++ src/main/java/oogasalad/Frontend/Menu/HomeView.java
@@ -1,14 +1,14 @@
 package oogasalad.Frontend.Menu;
 
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import oogasalad.Frontend.MainView;
 import oogasalad.Frontend.SceneView;
+
+import java.util.Arrays;
 
 /**
  * HomeView class will handle the navigation of the User from the home screen to the next page they seek.
@@ -17,31 +17,43 @@ import oogasalad.Frontend.SceneView;
 
 public class HomeView extends SceneView {
 
+    private VBox myVbox;
+    private static final String[] ButtonOptions = {"HomeViewCreate", "HomeViewJoin", "HomeViewHost"};
+    private static final String TITLE = "HomeViewTitle";
+
+    public HomeView() {
+    }
+
     @Override
     protected Scene makeScene() {
+
         Scene scene = new Scene(myRoot);
+        myVbox = new VBox();
+
         myRoot.prefWidth(500);
         myRoot.prefHeight(700);
-        Text t = new Text("BETTER CALL TAL HOME SCREEN");
+
+        Text t = new Text(MainView.getLanguage().getString(TITLE));
         t.setFont(new Font(64));
-        VBox vBox = new VBox();
-        vBox.getChildren().add(t);
-        myRoot.getChildren().add(vBox);
+        myVbox.getChildren().add(t);
 
         Button[] buttons = makeButtons();
-        VBox.setVgrow(buttons[0], Priority.ALWAYS);
-        VBox.setVgrow(buttons[1], Priority.ALWAYS);
-        VBox.setVgrow(buttons[2], Priority.ALWAYS);
 
-        vBox.getChildren().addAll(buttons);
+        // TODO: Figure out setVgrow
+        // VBox.setVgrow(buttons[0], Priority.ALWAYS);
+        // VBox.setVgrow(buttons[1], Priority.ALWAYS);
+        // VBox.setVgrow(buttons[2], Priority.ALWAYS);
+
+        myVbox.getChildren().addAll(buttons);
+        myRoot.getChildren().add(myVbox);
 
         return scene;
     }
 
     private Button[] makeButtons() {
-        Button[] ret = new Button[3];
-        for (int i=0; i <3; i++) {
-            ret[i] = new Button(String.valueOf(i));
+        Button[] ret = new Button[ButtonOptions.length];
+        for (int i=0; i < ButtonOptions.length; i++) {
+            ret[i] = new Button(MainView.getLanguage().getString(ButtonOptions[i]));
             ret[i].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         }
         return ret;
