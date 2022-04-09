188ea4d5175580274007e0591dfc9ab5518be52a
Leo Cao <lhc22@duke.edu>
2022-04-09 17:46:01 -0400

    refactored set piece infinite movement

--- src/main/java/oogasalad/Editor/ModelState/EditPiece/EditPieceGrid.java
+++ src/main/java/oogasalad/Editor/ModelState/EditPiece/EditPieceGrid.java
@@ -20,63 +20,17 @@ public class EditPieceGrid {
   }
 
   public void setTileInfinite(int dirX, int dirY){
-    if(dirX == 1 && dirY == 0){
-      int row = 3;
-      for(int i = 4; i < PIECE_GRID_SIZE; i++){
-        pieceGrid[row][i] = PieceGridTile.OPEN;
-        if(i == PIECE_GRID_SIZE - 1){
-          pieceGrid[row][i] = PieceGridTile.INFINITY;
-        }
-      }
+    if(dirX == 0 && dirY == 0){
+      return;
     }
-    else if(dirX == -1 && dirY == 0){
-      int row = 3;
-      for(int i = 2; i >= 0;i--){
-        pieceGrid[row][i] = PieceGridTile.OPEN;
-        if(i == 0){
-          pieceGrid[row][i] = PieceGridTile.INFINITY;
-        }
-      }
+    int row = 3-dirY;
+    int col = 3+dirX;
+    while(row > 0 && row < PIECE_GRID_SIZE-1 && col > 0 && col < PIECE_GRID_SIZE-1){
+      pieceGrid[row][col] = PieceGridTile.OPEN;
+      row -= dirY;
+      col += dirX;
     }
-    else if(dirX == 0 || dirY == 0){
-      int row = 3-dirY;
-      int col = 3+dirX;
-      while(row > 0 && row < PIECE_GRID_SIZE-1 && col > 0 && col < PIECE_GRID_SIZE-1){
-        pieceGrid[row][col] = PieceGridTile.OPEN;
-        row -= dirY;
-        col += dirX;
-      }
-      pieceGrid[row][col] = PieceGridTile.INFINITY;
-    }
-    else if(dirX == 0 && dirY == 1){
-      int column = 3;
-      for(int i = 2; i >= 0; i--){
-        pieceGrid[i][column] = PieceGridTile.OPEN;
-        if(i == 0){
-          pieceGrid[i][column] = PieceGridTile.INFINITY;
-        }
-      }
-    }
-    else if(dirX == 0 && dirY == -1){
-      int column = 3;
-      for(int i = 4; i < PIECE_GRID_SIZE; i++){
-        pieceGrid[i][column] = PieceGridTile.OPEN;
-        if(i == PIECE_GRID_SIZE - 1){
-          pieceGrid[i][column] = PieceGridTile.INFINITY;
-        }
-      }
-    }
-    //diagonal
-    else{
-      int col = 3 + dirX;
-      int row = 3 - dirY;
-      while(row > 0 && row < PIECE_GRID_SIZE-1){
-        pieceGrid[row][col] = PieceGridTile.OPEN;
-        row -= dirY;
-        col += dirX;
-      }
-      pieceGrid[row][col] = PieceGridTile.INFINITY;
-    }
+    pieceGrid[row][col] = PieceGridTile.INFINITY;
   }
 
   private void initializePieceBoard(){
