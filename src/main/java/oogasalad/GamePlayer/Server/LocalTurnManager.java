de335d0986179db5f5114bab4842c0f32a732f32
Ritvik Janamsetty <janamsetty@gmail.com>
2022-04-09 18:08:04 -0400

    Removed Interactionm handler and refactored into LocalTurnManager

--- src/main/java/oogasalad/GamePlayer/Server/LocalTurnManager.java
+++ src/main/java/oogasalad/GamePlayer/Server/LocalTurnManager.java
@@ -1,25 +1,58 @@
 package oogasalad.GamePlayer.Server;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 import oogasalad.GamePlayer.Board.ChessBoard;
-import oogasalad.GamePlayer.Board.EndConditionChecker;
+import oogasalad.GamePlayer.Board.EndConditions.EndCondition;
+import oogasalad.GamePlayer.Board.Player;
 import oogasalad.GamePlayer.Board.TurnCriteria.TurnCriteria;
 
 /**
  * Manages all turn interactions in a local game. To implement the same interact regardless whether
  * the game is played locally or remotely, this class has rather simple methods with each method
- * simply calling the corresponding method in the appropriate object.
+ * simply calling the corresponding method in the appropriate object. Thus, this class is
+ * intentionally designed not be active and serve as nothing more than a wrapper for the
+ * PlayerInteractionHandler to allow a same interface for both local and remote games.
  */
 public class LocalTurnManager implements TurnManager {
 
-  private final ChessHistory history;
-  private final TurnCriteria turn;
-  private final EndConditionChecker endChecker;
+  private PriorityQueue<EndCondition> endConditions;
+  private TurnCriteria turn;
+  private Player[] players;
+  private int[] teamNums;
+  private ChessHistory history;
+  private Map<Integer, Double> endResult;
 
   //TO-DO: implement a builder pattern for this class once class finalization is complete
-  public LocalTurnManager(ChessHistory history, TurnCriteria turn, EndConditionChecker endChecker) {
+  public LocalTurnManager(ChessHistory history, Player[] players, TurnCriteria turn,
+      EndCondition... conditions) {
+    this(history, players, turn, Arrays.asList(conditions));
+  }
+
+  public LocalTurnManager(ChessHistory history, Player[] players, TurnCriteria turn,
+      Iterable<EndCondition> conditions) {
     this.history = history;
+    this.players = players;
     this.turn = turn;
-    this.endChecker = endChecker;
+    teamNums = getTeamNums(players);
+    endConditions = new PriorityQueue<>();
+    for (EndCondition condition : conditions) {
+      endConditions.add(condition);
+    }
+    endResult = new HashMap<>();
+  }
+
+  /**
+   * Adds a new EndCondition to the end of the priority of EndConditions to check.
+   *
+   * @param condition the EndCondition to add
+   */
+  @Override
+  public void addEndCondition(EndCondition condition) {
+    endConditions.add(condition);
   }
 
   /**
@@ -89,6 +122,65 @@ public class LocalTurnManager implements TurnManager {
    */
   @Override
   public boolean isGameOver(ChessBoard board) {
-    return endChecker.isGameOver(board);
+    for (EndCondition ec : endConditions) {
+      Map<Integer, Double> result = ec.getScores(board);
+      if (!result.isEmpty()) {
+        endResult = result;
+        return true;
+      }
+    }
+    return false;
   }
+
+  /**
+   * Gets an immutable map of scores of all players after game over. If game isn't over, an empty
+   * map is returned.
+   *
+   * @return scores of all players after game over.
+   */
+  @Override
+  public Map<Integer, Double> getScores() {
+    return Collections.unmodifiableMap(endResult);
+  }
+
+  /**
+   * Gets the player object with the associated ID
+   *
+   * @param id of player
+   * @return player with given id
+   */
+  @Override
+  public Player getPlayer(int id) {
+    return players[Math.min(id, players.length - 1)];
+  }
+
+  /**
+   * Get array containing all the players
+   *
+   * @return players list
+   */
+  @Override
+  public Player[] getPlayers() {
+    return players;
+  }
+
+  /**
+   * Gets all team numbers
+   *
+   * @return team numbers for all players
+   */
+  @Override
+  public int[] getTeams() {
+    return teamNums;
+  }
+
+  /**
+   * Convert players to team numbers
+   *
+   * @return team nums associated with each player
+   */
+  private int[] getTeamNums(Player[] players) {
+    return Arrays.stream(players).mapToInt(Player::teamID).toArray();
+  }
+
 }
