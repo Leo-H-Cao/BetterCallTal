25b7222f28165f82b24ba3b1378e45cf5bda1df2
Leo Cao <lhc22@duke.edu>
2022-04-09 18:19:00 -0400

    editor edit single piece api

--- src/main/java/oogasalad/Editor/ModelState/EditPiece/EditSinglePiece.java
+++ src/main/java/oogasalad/Editor/ModelState/EditPiece/EditSinglePiece.java
@@ -1,5 +1,27 @@
 package oogasalad.Editor.ModelState.EditPiece;
 
 public class EditSinglePiece {
+  private EditPieceGrid editPieceGrid;
+
+  public EditSinglePiece(){
+    editPieceGrid = new EditPieceGrid();
+  }
+
+  public void setTileOpen(int x, int y){
+    editPieceGrid.setTileOpen( x, y);
+  }
+
+  public void setTileClosed(int x, int y){
+    editPieceGrid.setTileClosed(x, y);
+  }
+
+  public void setTileInfinite(int dirX, int dirY){
+    editPieceGrid.setTileInfinite(dirX, dirY);
+  }
+
+  //TODO
+  public void setPieceModifier(){
+
+  }
 
 }
