e2a9bfe41ffc987dd23eae18cdd12521c50711d5
Ritvik Janamsetty <rj160@duke.edu>
2022-04-10 11:24:20 -0400

    Update GamePLayer to include team numbers support

--- src/main/java/oogasalad/GamePlayer/Board/GamePlayers.java
+++ src/main/java/oogasalad/GamePlayer/Board/GamePlayers.java
@@ -8,10 +8,21 @@ import java.util.List;
  * This class is used to hold the players in the game and their information. Has methods to access
  * different information about the players.
  *
- * @param players The players in the game
+ * @param players     The players in the game
+ * @param teamNumbers the team numbers of the players
  * @author Ritvik Janamsetty
  */
-public record GamePlayers(List<Player> players) {
+public record GamePlayers(List<Player> players, List<Integer> teamNumbers) {
+
+  /**
+   * This class is used to hold the players in the game and their information. Has methods to access
+   * different information about the players.
+   *
+   * @param players The players in the game
+   */
+  public GamePlayers(List<Player> players) {
+    this(players, getTeamNums(players));
+  }
 
   /**
    * This class is used to hold the players in the game and their information. Has methods to access
@@ -48,6 +59,15 @@ public record GamePlayers(List<Player> players) {
   }
 
   /**
+   * Convert players to team numbers
+   *
+   * @return team nums associated with each player
+   */
+  private static List<Integer> getTeamNums(List<Player> players) {
+    return players.stream().mapToInt(Player::teamID).boxed().toList();
+  }
+
+  /**
    * Gets the player object with the associated ID
    *
    * @param id of player
@@ -72,8 +92,6 @@ public record GamePlayers(List<Player> players) {
    * @return team numbers for all players
    */
   public int[] getTeams() {
-    return players.stream().mapToInt(Player::teamID).toArray();
+    return teamNumbers.stream().mapToInt(Integer::intValue).toArray();
   }
-
-
 }
