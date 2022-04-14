1d801b77c3855ba17ce9b40405116f21961ad151
Vincent Chen <vjc9@duke.edu>
2022-04-13 22:56:32 -0400

    bank leaver implementation #034

--- src/main/java/oogasalad/GamePlayer/Movement/CustomMovements/BankLeaver.java
+++ src/main/java/oogasalad/GamePlayer/Movement/CustomMovements/BankLeaver.java
@@ -1,7 +1,14 @@
 package oogasalad.GamePlayer.Movement.CustomMovements;
 
+import static oogasalad.GamePlayer.ValidStateChecker.BankBlocker.BLOCK_COL;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.Tiles.ChessTile;
 import oogasalad.GamePlayer.EngineExceptions.InvalidMoveException;
@@ -9,40 +16,65 @@ import oogasalad.GamePlayer.EngineExceptions.OutsideOfBoardException;
 import oogasalad.GamePlayer.GamePiece.Piece;
 import oogasalad.GamePlayer.Movement.Coordinate;
 import oogasalad.GamePlayer.Movement.MovementInterface;
+import oogasalad.GamePlayer.ValidStateChecker.BankBlocker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+/***
+ * Creates a custom movement that allows pieces to leave the bank
+ */
 public class BankLeaver implements MovementInterface {
 
-  /***
-   * Creates a custom movement that allows pieces to leave the bank
+  private static final Logger LOG = LogManager.getLogger(BankLeaver.class);
+
+
+  /**
+   * @return updated squares when a piece leaves the bank
    */
-  public BankLeaver() {
-
-  }
-
   @Override
   public Set<ChessTile> movePiece(Piece piece, Coordinate finalSquare, ChessBoard board)
       throws InvalidMoveException, OutsideOfBoardException {
-    return null;
+    if(!getMoves(piece, board).contains(board.getTile(finalSquare))) {
+      LOG.warn("Illegal bank leaving move attempted");
+      throw new InvalidMoveException(finalSquare.toString());
+    }
+    Set<ChessTile> updatedSquares = new HashSet<>(
+        Arrays.asList(board.getTile(piece.getCoordinates()), board.getTile(finalSquare)));
+    updatedSquares.addAll(piece.updateCoordinates(board.getTile(finalSquare), board));
+    return updatedSquares;
   }
 
   @Override
   public Set<ChessTile> capturePiece(Piece piece, Coordinate captureSquare, ChessBoard board)
       throws InvalidMoveException, OutsideOfBoardException {
-    return null;
+    LOG.warn("Bank leaving does not support captures");
+    throw new InvalidMoveException("Bank leaving does not support captures");
   }
 
+  /***
+   * @return nothing, not applicable
+   */
   @Override
   public Set<ChessTile> getCaptures(Piece piece, ChessBoard board) {
-    return null;
+    return Collections.emptySet();
   }
 
+  /***
+   * @return all open squares on board to the left of the bank, if piece is in the bank
+   */
   @Override
   public Set<ChessTile> getMoves(Piece piece, ChessBoard board) {
-    return null;
+    if(piece.getCoordinates().getCol() < BLOCK_COL) return Set.of();
+    return board.stream().flatMap(Collection::stream).toList().stream().filter(t ->
+        t.getCoordinates().getCol() < BLOCK_COL && t.getPieces().isEmpty())
+        .collect(Collectors.toSet());
   }
 
+  /***
+   * @return nothing, not applicable
+   */
   @Override
   public List<Coordinate> getRelativeCoords() {
-    return null;
+    return Collections.emptyList();
   }
 }
---
+++ src/main/java/oogasalad/GamePlayer/Movement/MovementModifiers/BankJoiner.java
@@ -0,0 +1,14 @@
+package oogasalad.GamePlayer.Movement.MovementModifiers;
+
+import java.util.Set;
+import oogasalad.GamePlayer.Board.ChessBoard;
+import oogasalad.GamePlayer.Board.Tiles.ChessTile;
+import oogasalad.GamePlayer.GamePiece.Piece;
+
+public class BankJoiner implements MovementModifier{
+
+  @Override
+  public Set<ChessTile> updateMovement(Piece piece, ChessBoard board) {
+    return null;
+  }
+}
