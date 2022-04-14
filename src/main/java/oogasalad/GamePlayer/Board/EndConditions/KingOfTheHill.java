ef36e4bda7a0cfca4ac40896796f7a969a3c1758
Vincent Chen <vjc9@duke.edu>
2022-04-14 11:54:56 -0400

    KOTH implementation #041

---
+++ src/main/java/oogasalad/GamePlayer/Board/EndConditions/KingOfTheHill.java
@@ -0,0 +1,75 @@
+package oogasalad.GamePlayer.Board.EndConditions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.IntStream;
+import oogasalad.GamePlayer.Board.ChessBoard;
+import oogasalad.GamePlayer.Board.Tiles.ChessTile;
+import oogasalad.GamePlayer.EngineExceptions.OutsideOfBoardException;
+import oogasalad.GamePlayer.Movement.Coordinate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/***
+ * End condition where first person to reach the middle of the board wins
+ */
+public class KingOfTheHill implements EndCondition {
+
+  private static final Logger LOG = LogManager.getLogger(KingOfTheHill.class);
+
+  /***
+   * @param board to get scores from
+   * @return WIN for king in the middle, LOSS for others, empty map if not applicable
+   */
+  @Override
+  public Map<Integer, Double> getScores(ChessBoard board) {
+    Set<ChessTile> middleTiles = getMiddleTiles(board);
+    Map<Integer, Double> scores = new HashMap<>();
+
+    middleTiles.stream().filter((t) -> t.getPiece().isPresent() && t.getPiece().get().isTargetPiece())
+        .findFirst().ifPresent((t) -> {
+          int winningTeam = t.getPiece().get().getTeam();
+          scores.put(winningTeam, WIN);
+          Arrays.stream(board.getPlayer(winningTeam).opponentIDs()).forEach(team ->
+              scores.put(team, LOSS));
+        });
+
+    if(scores.isEmpty()) return scores;
+
+    Arrays.stream(board.getPlayers()).filter((p) -> !scores.containsKey(p.teamID())).forEach(pl ->
+        scores.put(pl.teamID(), DRAW));
+
+    return scores;
+  }
+
+  /***
+   * @return the middle tiles in the board
+   */
+  private Set<ChessTile> getMiddleTiles(ChessBoard board) {
+    int[] colRange = getRange(board.getBoardLength());
+    int[] rowRange = getRange(board.getBoardHeight());
+    Set<ChessTile> middleTiles = new HashSet<>();
+
+    IntStream.range(rowRange[0], rowRange[1]+1).forEach((i) ->
+        IntStream.range(colRange[0], colRange[1]+1).forEach((j) ->
+        { try {
+            middleTiles.add(board.getTile(Coordinate.of(i, j)));
+          } catch (OutsideOfBoardException ignored) {
+            LOG.debug("Middle tile out of bounds");
+          }
+        }));
+    LOG.debug(String.format("Middle tiles: %s", middleTiles));
+    return middleTiles;
+  }
+
+  /**
+   * @param max length of section
+   * @return middle element if odd, middle two if even
+   */
+  private int[] getRange(int max) {
+    return max % 2 == 0 ? new int[]{max/2-1, max/2} : new int[]{max/2, max/2};
+  }
+}
