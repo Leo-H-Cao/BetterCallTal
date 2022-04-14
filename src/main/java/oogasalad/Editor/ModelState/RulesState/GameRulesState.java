6e1cd9a19fb8d3487c011f85fbd2a999bfaff7a7
Leo Cao <lhc22@duke.edu>
2022-04-13 22:37:35 -0400

    removed redundant game rules class

--- src/main/java/oogasalad/Editor/ModelState/RulesState/GameRulesState.java
+++ src/main/java/oogasalad/Editor/ModelState/RulesState/GameRulesState.java
@@ -1,25 +1,59 @@
 package oogasalad.Editor.ModelState.RulesState;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
+/**
+ * @author Leo Cao
+ * Tracks game rules configurations changes from user
+ */
 public class GameRulesState {
+  private static final String CONFIGURATION_RESOURCE_PATH = "oogasalad/Editor/GameRules";
 
-  private GameRules myGameRules;
+  private List<String> winConditions;
+  private String turnCriteria;
+  private ResourceBundle myResources;
 
   public GameRulesState(){
-    myGameRules = new GameRules();
+    setResources();
+    winConditions = new ArrayList<>();
+    winConditions.add(myResources.getString("defaultConditionsCheckmate"));
+    winConditions.add(myResources.getString("defaultConditionsStalemate"));
+    turnCriteria = myResources.getString("defaultTurnCriteria");
   }
 
+  /**
+   * Tracks win conditions for the game that is being created
+   * @param winConditions list of win conditions
+   */
   public void setWinConditions(List<String> winConditions) {
-    myGameRules.setWinConditions(winConditions);
+    this.winConditions = winConditions;
   }
 
-  public void setTurnCriteria(String turnCriteria) {
-    myGameRules.setTurnCriteria(turnCriteria);
+  /**
+   * Tracks turn criteria (default is linear) for current game being created
+   * @param turnCriteria string representing chosen turn criteria
+   */
+  public void setTurnCriteria(String turnCriteria){
+    this.turnCriteria = turnCriteria;
   }
 
-  //For testing
-  public GameRules getGameRules(){
-    return myGameRules;
+  /**
+   * Getter for turn criteria set by user
+   * @return string representing turn criteria
+   */
+  public String getTurnCriteria(){
+    return turnCriteria;
+  }
+
+  private void setResources() {
+    try {
+      myResources = ResourceBundle.getBundle(CONFIGURATION_RESOURCE_PATH, Locale.ENGLISH);
+    } catch (NullPointerException | MissingResourceException e) {
+      throw new IllegalArgumentException(String.format("Invalid resource file: %s", "GameRules"));
+    }
   }
 }
