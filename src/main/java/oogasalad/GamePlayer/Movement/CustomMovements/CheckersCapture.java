baa8a91934ad3a17708cd297f3afb239996c5be5
Vincent Chen <vjc9@duke.edu>
2022-04-16 17:42:18 -0400

    logic for checkers capture implemente but untested #047

--- src/main/java/oogasalad/GamePlayer/Movement/CustomMovements/CheckersCapture.java
+++ src/main/java/oogasalad/GamePlayer/Movement/CustomMovements/CheckersCapture.java
@@ -1,8 +1,12 @@
 package oogasalad.GamePlayer.Movement.CustomMovements;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.Tiles.ChessTile;
 import oogasalad.GamePlayer.EngineExceptions.InvalidMoveException;
@@ -16,6 +20,8 @@ import org.apache.logging.log4j.Logger;
 public class CheckersCapture implements MovementInterface {
 
   private static final Logger LOG = LogManager.getLogger(CheckersCapture.class);
+
+  private Map<Coordinate, Set<ChessTile>> capturePaths = new HashMap<>();
 
   /**
    * @return exception because no capture possible
@@ -35,7 +41,21 @@ public class CheckersCapture implements MovementInterface {
   @Override
   public Set<ChessTile> capturePiece(Piece piece, Coordinate captureSquare, ChessBoard board)
       throws InvalidMoveException, OutsideOfBoardException {
-    return Collections.emptySet();
+
+    capturePaths = new HashMap<>();
+
+    if (!getCaptures(piece, board).contains(board.getTile(captureSquare))) {
+      LOG.warn("Illegal checkers capture move attempted");
+      throw new InvalidMoveException(captureSquare.toString());
+    }
+
+    Set<ChessTile> updatedSquares = new HashSet<>(List.of(board.getTile(captureSquare)));
+    capturePaths.get(captureSquare).forEach(t -> {
+      t.clearPieces();
+      updatedSquares.add(t);
+    });
+    piece.updateCoordinates(board.getTile(captureSquare), board);
+    return updatedSquares;
   }
 
   /**
@@ -43,8 +63,68 @@ public class CheckersCapture implements MovementInterface {
    */
   @Override
   public Set<ChessTile> getCaptures(Piece piece, ChessBoard board) {
-    List<Coordinate> directions = piece.getRelativeMoveCoords();
-    return Collections.emptySet();
+    return generateCapturesTree(board, piece.getCoordinates(), piece.getRelativeMoveCoords(),
+        piece.getTeam(), new HashSet<>(), new HashSet<>());
+  }
+
+  /***
+   * Generates capture tree by branching off from starting position outward while captures are
+   * possible
+   *
+   * @param board to get captures on
+   * @param startCoordinate of piece
+   * @param directions that the piece can move in
+   * @param team of piece
+   * @param currentJumps is the list of current captures
+   *
+   * @return all capture squares
+   */
+  private Set<ChessTile> generateCapturesTree(ChessBoard board, Coordinate startCoordinate,
+      List<Coordinate> directions, int team,
+      Set<ChessTile> currentJumps, Set<ChessTile> capTiles) {
+    AtomicBoolean atEndOfPath = new AtomicBoolean(true);
+    directions.stream().filter(d -> canCapture(board, startCoordinate, d, team)).forEach(d -> {
+      atEndOfPath.set(false);
+      Coordinate capTile = Coordinate.add(d, startCoordinate);
+      Coordinate jumpTile = Coordinate.add(d, capTile);
+      Set<ChessTile> copyJump = new HashSet<>(currentJumps);
+      Set<ChessTile> copyCap = new HashSet<>(capTiles);
+
+      try {copyJump.add(board.getTile(jumpTile)); copyCap.add(board.getTile(capTile));}
+      catch (OutsideOfBoardException ignored) {LOG.debug("Capture out of bounds");}
+      generateCapturesTree(board, jumpTile, directions, team,
+          copyJump, copyCap);
+    });
+
+    if(atEndOfPath.get()) {
+      capturePaths.put(startCoordinate, capTiles);
+    }
+
+    return currentJumps;
+  }
+
+  /**
+   * Returns if a capture is possible based on checkers rules
+   *
+   * @param board to jump on
+   * @param startCord starting square
+   * @param direction to jump in
+   * @return if a capture is possible
+   */
+  private boolean canCapture(ChessBoard board, Coordinate startCord, Coordinate direction, int team) {
+
+    Coordinate passOverCord = Coordinate.add(direction, startCord);
+    Coordinate finalCord = Coordinate.add(direction, passOverCord);
+
+    if(!board.inBounds(passOverCord) || !board.inBounds(finalCord)) return false;
+
+    try {
+      return board.getTile(passOverCord).getPiece().isPresent() &&
+          board.isOpposing(board.getTile(passOverCord), team) &&
+          board.getTile(finalCord).getPiece().isEmpty();
+    } catch (OutsideOfBoardException e) {
+      return false;
+    }
   }
 
   /**
