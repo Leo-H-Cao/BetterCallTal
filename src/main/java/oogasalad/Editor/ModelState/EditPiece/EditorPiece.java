2d8212160def2782c7bd7f3e9ae5e438e610fb2d
Nolan Gelinas <nolangelinas@gmail.com>
2022-04-17 02:19:48 -0400

    Add new custom pieces show up and update images in board editor

--- src/main/java/oogasalad/Editor/ModelState/EditPiece/EditorPiece.java
+++ src/main/java/oogasalad/Editor/ModelState/EditPiece/EditorPiece.java
@@ -99,4 +99,9 @@ public class EditorPiece {
   public void setPieceName(String pieceName) {
     this.pieceName = pieceName;
   }
+
+  @Override
+  public String toString() {
+    return pieceID;
+  }
 }
