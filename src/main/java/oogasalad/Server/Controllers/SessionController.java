ad76d5334852e858f3cdc880ca56fa385af5e246
Ritvik Janamsetty <rj160@duke.edu>
2022-04-18 02:47:59 -0400

    Create a session controller

---
+++ src/main/java/oogasalad/Server/Controllers/SessionController.java
@@ -0,0 +1,82 @@
+package oogasalad.Server.Controllers;
+
+import oogasalad.GamePlayer.Board.ChessBoard;
+import oogasalad.Server.SessionManagement.GameSessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+
+  private final GameSessionService activeSessions;
+
+  /**
+   * Constructor called by Spring to create a new HistoryController. Constructor initializes the
+   * autowired
+   *
+   * @param activeSessions the game session service that is used to get the active game sessions.
+   */
+  @Autowired
+  public SessionController(GameSessionService activeSessions) {
+    this.activeSessions = activeSessions;
+  }
+
+  /**
+   * Creates a new game session with the given id.
+   *
+   * @param id           the id of the game session
+   * @param initialBoard the initial board of the game session
+   */
+  @PostMapping("/host/{id}/{host}/{opponent}")
+  public void createGameSession(@PathVariable String id, @PathVariable int host,
+      @PathVariable int opponent, ChessBoard initialBoard) {
+    activeSessions.addSession(id, host, opponent, initialBoard);
+  }
+
+  /**
+   * Joins the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  @GetMapping("/join/{id}")
+  public int joinGameSession(@PathVariable String id) {
+    return activeSessions.getSession(id).getOpponent();
+  }
+
+  /**
+   * Pauses the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  @PutMapping("/pause/{id}")
+  public void pauseGameSession(@PathVariable String id) {
+    activeSessions.getSession(id).setPaused(true);
+  }
+
+  /**
+   * Resumes the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  @PutMapping("/resume/{id}")
+  public void resumeGameSession(@PathVariable String id) {
+    activeSessions.getSession(id).setPaused(false);
+  }
+
+  /**
+   * Ends the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  @DeleteMapping("/end/{id}")
+  public void endGameSession(@PathVariable String id) {
+    activeSessions.removeSession(id);
+  }
+}
