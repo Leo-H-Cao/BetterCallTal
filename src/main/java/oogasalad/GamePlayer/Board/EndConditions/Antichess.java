09c0144c591f23a4d3567d5354728d2f03071288
Vincent Chen <vjc9@duke.edu>
2022-04-18 15:07:27 -0400

    antichess implementation by extending loseallpieces and inverting the map #052

--- src/main/java/oogasalad/GamePlayer/Board/EndConditions/Antichess.java
+++ src/main/java/oogasalad/GamePlayer/Board/EndConditions/Antichess.java
@@ -5,10 +5,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.Player;
 
-public class Antichess implements EndCondition {
+public class Antichess extends LoseAllPieces {
 
   /***
    * Losers are the ones who lose all pieces, winners are their opponents, draw for everyone else
@@ -18,7 +19,22 @@ public class Antichess implements EndCondition {
    */
   @Override
   public Map<Integer, Double> getScores(ChessBoard board) {
-    return null;
+    Map<Integer, Double> invertedMap = super.getScores(board);
+    return invertedMap.keySet().stream().collect(Collectors.toMap(k -> k,
+        k -> invertScore(invertedMap.get(k))));
+  }
+
+  /***
+   * Inverts the given score with the equation y = 1-x
+   * LOSS (0) -> WIN (1)
+   * DRAW (0.5) -> DRAW (0.5)
+   * WIN (1) -> LOSS (0)
+   *
+   * @param val to invert
+   * @return 1-val
+   */
+  private double invertScore(double val) {
+    return WIN-val;
   }
 
   /***
