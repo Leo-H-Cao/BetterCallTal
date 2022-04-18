5083bcb17454174e63877b052712f9d217e29518
Jed Yang <zy111@duke.edu>
2022-04-18 00:28:07 -0400

    addid King of the Hill tests

---
+++ src/test/java/oogasalad/GamePlayer/Board/BoardTestUtil.java
@@ -0,0 +1,39 @@
+package oogasalad.GamePlayer.Board;
+
+
+import java.util.Collections;
+import java.util.List;
+import oogasalad.GamePlayer.GamePiece.Piece;
+import oogasalad.GamePlayer.GamePiece.PieceData;
+import oogasalad.GamePlayer.Movement.Coordinate;
+import oogasalad.GamePlayer.Movement.Movement;
+
+/***
+ * This class contains util functions that speed up the testing process for Board classes
+ */
+
+public class BoardTestUtil {
+
+  public Piece makeKing(int row, int col, int team) {
+    return new Piece(new PieceData(new Coordinate(row, col),
+        "king" + team, 0, team, true,
+        List.of(new Movement(List.of(new Coordinate(1, 0)), false)), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), ""));
+  }
+
+  public Piece makeRook(int row, int col, int team) {
+    Movement rookMovement = new Movement(List.of(
+        new Coordinate(1, 0),
+        new Coordinate(-1, 0),
+        new Coordinate(0, 1),
+        new Coordinate(0, -1)),
+        true);
+
+    return new Piece(new PieceData(new Coordinate(row, col),
+        "rook" + team, 0, team, false,
+        List.of(rookMovement),
+        List.of(rookMovement), Collections.emptyList(),
+        Collections.emptyList(), ""));
+  }
+
+
+}
---
+++ src/test/java/oogasalad/GamePlayer/Board/EndConditions/KingOfTheHillTest.java
@@ -0,0 +1,73 @@
+package oogasalad.GamePlayer.Board.EndConditions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import oogasalad.GamePlayer.Board.BoardTestUtil;
+import oogasalad.GamePlayer.Board.ChessBoard;
+import oogasalad.GamePlayer.Board.Player;
+import oogasalad.GamePlayer.Board.TurnCriteria.Linear;
+import oogasalad.GamePlayer.GamePiece.Piece;
+import oogasalad.GamePlayer.ValidStateChecker.Check;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class KingOfTheHillTest {
+  private BoardTestUtil util;
+
+  private static final Logger LOG = LogManager.getLogger(CheckmateTest.class);
+
+  private static final int TEAM_1 = 1;
+  private static final int TEAM_2 = 0;
+
+  oogasalad.GamePlayer.ValidStateChecker.Check Check = new Check();
+  Checkmate checkmate = new Checkmate();
+
+  private ChessBoard board;
+  private List<Piece> pieces;
+
+  @BeforeEach
+  void setUp() {
+
+    util = new BoardTestUtil();
+
+    Player playerOne = new Player(TEAM_1, new int[]{TEAM_2});
+    Player playerTwo = new Player(TEAM_2, new int[]{TEAM_1});
+    Player[] players = new Player[]{playerOne, playerTwo};
+
+    Linear turnCriteria = new Linear(players);
+    TwoMoves endCondition = new TwoMoves();
+
+    board = new ChessBoard(8, 8, turnCriteria, players, List.of(endCondition));
+    pieces = new ArrayList<>();
+  }
+
+
+  @Test
+  void Player1Wins(){
+    Piece wking = util.makeKing(4, 4, TEAM_1);
+    Piece bking = util.makeKing(1, 1, TEAM_2);
+
+    pieces.addAll(List.of(wking, bking));
+    board.setPieces(pieces);
+
+    System.out.println(new KingOfTheHill().getScores(board).get(0));
+    System.out.println(new KingOfTheHill().getScores(board).get(1));
+
+    assertTrue(new KingOfTheHill().getScores(board).get(TEAM_1) == 1.0);
+  }
+
+  @Test
+  void Player2Wins(){
+
+  }
+
+  @Test
+  void NoWin(){
+
+  }
+
+}
