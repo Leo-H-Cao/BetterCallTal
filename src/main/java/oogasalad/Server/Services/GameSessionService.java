b9403c440b8b34c302d43b4cfc8d9332c799a877
Ritvik Janamsetty <rj160@duke.edu>
2022-04-18 00:36:39 -0400

    created api service for managing game sessions

---
+++ src/main/java/oogasalad/Server/Services/GameSessionService.java
@@ -0,0 +1,40 @@
+package oogasalad.Server.Services;
+
+public interface GameSessionService {
+
+  /**
+   * Creates a new game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  void createGameSession(String id);
+
+  /**
+   * Joins the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  void joinGameSession(String id);
+
+  /**
+   * Pauses the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  void pauseGameSession(String id);
+
+  /**
+   * Resumes the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  void resumeGameSession(String id);
+
+  /**
+   * Ends the game session with the given id.
+   *
+   * @param id the id of the game session
+   */
+  void endGameSession(String id);
+
+}
