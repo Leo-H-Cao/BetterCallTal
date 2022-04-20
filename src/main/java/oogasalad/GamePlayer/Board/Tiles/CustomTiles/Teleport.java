169dc3e2bae9dec1cf9ce8b989006e2f2a631e6b
Vincent Chen <vjc9@duke.edu>
2022-04-19 20:45:17 -0400

    file read for teleport for custom teleport locations #055

--- src/main/java/oogasalad/GamePlayer/Board/Tiles/CustomTiles/Teleport.java
+++ src/main/java/oogasalad/GamePlayer/Board/Tiles/CustomTiles/Teleport.java
@@ -1,11 +1,20 @@
 package oogasalad.GamePlayer.Board.Tiles.CustomTiles;
 
+import static oogasalad.GamePlayer.Board.Setup.BoardSetup.JSON_EXTENSION;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.Tiles.ChessTile;
 import oogasalad.GamePlayer.EngineExceptions.EngineException;
 import oogasalad.GamePlayer.Movement.Coordinate;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /***
  * Teleports piece to given square
@@ -14,9 +23,29 @@ import oogasalad.GamePlayer.Movement.Coordinate;
  */
 public class Teleport implements TileAction {
 
+  private static final String TP_FILE_PATH_HEADER = "doc/GameEngineResources/Other/teleportLocations/";
+  private static final String DEFAULT_TP_FILE = "teleport00";
+  private static final Coordinate DEFAULT_TP = Coordinate.of(0, 0);
+
   private Coordinate teleportLocation;
 
-  public Teleport
+  public Teleport() {
+    this(DEFAULT_TP_FILE);
+  }
+
+  public Teleport(String configFile) {
+    configFile = TP_FILE_PATH_HEADER + configFile + JSON_EXTENSION;
+    try {
+      String content = new String(Files.readAllBytes(
+          Path.of(configFile)));
+      JSONObject data = new JSONObject(content);
+      teleportLocation = Coordinate.of(data.getJSONArray("tile").getInt(0),
+          data.getJSONArray("tile").getInt(1));
+    } catch (IOException e) {
+      teleportLocation = DEFAULT_TP;
+    }
+  }
+
   /***
    * Teleports piece on the tile to the location determined by file read if the teleport square
    * is open
