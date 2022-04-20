e779b6073fae72e02e94110ac48ec7a4c73e7951
Vincent Chen <vjc9@duke.edu>
2022-04-19 21:30:30 -0400

    logic for snake implemented with one limitation: secondary move can only be applied once per move, meaning very narrow boards will return an empty set when it's intuitive to think that they wont #055

--- src/main/java/oogasalad/GamePlayer/Movement/CustomMovements/Snake.java
+++ src/main/java/oogasalad/GamePlayer/Movement/CustomMovements/Snake.java
@@ -23,6 +23,7 @@ public class Snake implements MovementInterface {
   private Coordinate primaryMoveDirection;
   private Coordinate secondaryMoveDirection;
   private Coordinate primaryMultiplier;
+  private boolean enactedSecondaryMove;
 
   /***
    * @return original tile and tile moved to
@@ -30,7 +31,18 @@ public class Snake implements MovementInterface {
   @Override
   public Set<ChessTile> movePiece(Piece piece, Coordinate finalSquare, ChessBoard board)
       throws InvalidMoveException, OutsideOfBoardException {
-    return null;
+    if(!getMoves(piece, board).contains(board.getTile(finalSquare))) {
+      LOG.warn("Illegal snake move attempted");
+      throw new InvalidMoveException(finalSquare.toString());
+    }
+
+    if(enactedSecondaryMove) {
+      primaryMoveDirection = Coordinate.multiply(primaryMoveDirection, primaryMultiplier);
+    }
+
+    ChessTile oldTile = board.getTile(piece.getCoordinates());
+    piece.updateCoordinates(board.getTile(finalSquare), board);
+    return Set.of(oldTile, board.getTile(piece.getCoordinates()));
   }
 
   /**
@@ -56,7 +68,31 @@ public class Snake implements MovementInterface {
    */
   @Override
   public Set<ChessTile> getMoves(Piece piece, ChessBoard board) {
-    return null;
+    Coordinate currentCoordinate = piece.getCoordinates();
+    Coordinate newCoordinate = Coordinate.add(currentCoordinate, primaryMoveDirection);
+    enactedSecondaryMove = false;
+
+    if(!board.inBounds(newCoordinate)) {
+      Coordinate delta = Coordinate.of(
+          findDelta(0, board.getBoardHeight(), newCoordinate.getRow()),
+          findDelta(0, board.getBoardLength(), newCoordinate.getCol()));
+      newCoordinate = Coordinate.add(Coordinate.add(newCoordinate, secondaryMoveDirection), delta);
+      enactedSecondaryMove = true;
+    }
+    try {
+      return Set.of(board.getTile(newCoordinate));
+    } catch (OutsideOfBoardException e) {
+      return Collections.emptySet();
+    }
+  }
+
+  /***
+   * Given a range min to max, find the difference needed to scale target into [min, max)
+   *
+   * @return above
+   */
+  private int findDelta(int min, int max, int target) {
+    return target < min ? min - target : (target >= max ? max - target - 1 : 0);
   }
 
   /***
