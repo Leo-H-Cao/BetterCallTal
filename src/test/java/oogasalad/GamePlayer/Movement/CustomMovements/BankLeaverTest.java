0f48d9f43a884a8194c4bb39a9802daf24a1c4fe
Vincent Chen <vjc9@duke.edu>
2022-04-20 21:33:54 -0400

    movement modifier test structure set up #061

--- src/test/java/oogasalad/GamePlayer/Movement/CustomMovements/BankLeaverTest.java
+++ src/test/java/oogasalad/GamePlayer/Movement/CustomMovements/BankLeaverTest.java
@@ -3,10 +3,10 @@ package oogasalad.GamePlayer.Movement.CustomMovements;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BankLeaverTest {
+public class BankLeaverTest {
 
   static final String BOARD_TEST_FILES_HEADER = "doc/testing_directory/test_boards/";
-  static final String BANK_TEST_FILE = BOARD_TEST_FILES_HEADER + "Crazyhouse.json";
+  public static final String BANK_TEST_FILE = BOARD_TEST_FILES_HEADER + "Crazyhouse.json";
 
   @BeforeEach
   void setUp() {
---
+++ src/test/java/oogasalad/GamePlayer/Movement/MovementModifiers/BankJoinerTest.java
@@ -0,0 +1,30 @@
+package oogasalad.GamePlayer.Movement.MovementModifiers;
+
+import static oogasalad.GamePlayer.Movement.CustomMovements.BankLeaverTest.BANK_TEST_FILE;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BankJoinerTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void testConfigFiles() {
+
+  }
+
+  @Test
+  void testCaptures() {
+
+  }
+
+  @Test
+  void testMoves() {
+
+  }
+
+}
