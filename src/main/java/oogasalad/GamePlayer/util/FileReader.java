19c4c2f73ea14416ac2c6b54d190d6a8c3ac64c2
Vincent Chen <vjc9@duke.edu>
2022-04-23 00:51:57 -0400

    en passant design fixes #068

--- src/main/java/oogasalad/GamePlayer/util/FileReader.java
+++ src/main/java/oogasalad/GamePlayer/util/FileReader.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import oogasalad.GamePlayer.Movement.Coordinate;
