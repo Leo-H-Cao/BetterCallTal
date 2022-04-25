1c57ece94b4e3785ae313e1c716558245b480129
Ritvik Janamsetty <rj160@duke.edu>
2022-04-24 22:29:17 -0400

    created a GameTYpe enum

---
+++ src/main/java/oogasalad/GamePlayer/Board/GameType.java
@@ -0,0 +1,12 @@
+package oogasalad.GamePlayer.Board;
+
+/**
+ * Enum for the different game types that can be played by the chessboard class.
+ */
+public enum GameType {
+
+  AI,
+  LOCAL,
+  SERVER
+
+}
