8d7f0269524995e74907cc4325f025dabbe1f765
Jed Yang <zy111@duke.edu>
2022-04-24 22:55:27 -0400

    added basic bot movement test

--- src/test/java/oogasalad/GamePlayer/ArtificialPlayer/BotTest.java
+++ src/test/java/oogasalad/GamePlayer/ArtificialPlayer/BotTest.java
@@ -1,13 +1,18 @@
 package oogasalad.GamePlayer.ArtificialPlayer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import oogasalad.Frontend.Game.TurnKeeper;
 import oogasalad.GamePlayer.Board.ChessBoard;
 import oogasalad.GamePlayer.Board.BoardSetup;
+import oogasalad.GamePlayer.GamePiece.Piece;
+import oogasalad.GamePlayer.Movement.Coordinate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BotTest {
-  Bot chessBot;
+  Bot gameBot;
   ChessBoard board;
 
 
@@ -15,12 +20,20 @@ public class BotTest {
   void setup() throws IOException {
     String JSONPath = "doc/testing_directory/AI_Testing/QueenBlunder.json";
     board = BoardSetup.createLocalBoard(JSONPath);
+    gameBot = new Bot(new TurnKeeper(new String[]{"human", "ai"}), "Hard");
 
   }
 
   @Test
-  void testBot(){
-    int a = 0/2;
+  void testBotIdentifiesQueenBlunder() throws Throwable {
+    List<Piece> pieces = board.getPieces();
+    for(Piece p : pieces){
+      if(p.getCoordinates().equals(new Coordinate(6, 7))){
+        board.move(p, new Coordinate(5, 7));
+        break;
+      };
+    }
+    gameBot.getBotMove(board, 1);
 
   }
 
