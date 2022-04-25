8848fe08dcbff6746635d524d2c28cb1c534e39f
Jed Yang <zy111@duke.edu>
2022-04-24 23:10:57 -0400

    made a more rigorous bot test

--- src/test/java/oogasalad/GamePlayer/ArtificialPlayer/BotTest.java
+++ src/test/java/oogasalad/GamePlayer/ArtificialPlayer/BotTest.java
@@ -1,5 +1,9 @@
 package oogasalad.GamePlayer.ArtificialPlayer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +28,23 @@ public class BotTest {
 
   }
 
+
   @Test
   void testBotIdentifiesQueenBlunder() throws Throwable {
     List<Piece> pieces = board.getPieces();
     for(Piece p : pieces){
       if(p.getCoordinates().equals(new Coordinate(6, 7))){
-        board.move(p, new Coordinate(5, 7));
+        board.move(p, new Coordinate(5, 7)); //player 0 makes a pawn move
         break;
       };
     }
+
+    //player 1's queen still on d3
+    assertEquals(1, board.getTile(new Coordinate(5, 3)).getPieces().size());
     gameBot.getBotMove(board, 1);
+
+    //bot should move player 1's queen away from attack
+    assertTrue(board.getTile(new Coordinate(5, 3)).getPieces().isEmpty());
 
   }
 
